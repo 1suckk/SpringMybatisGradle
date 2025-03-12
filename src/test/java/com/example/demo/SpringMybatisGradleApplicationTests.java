a2b76eadbf9a26d5ade707f9bde28969010af30a
1suckk <cwsgcd@gmail.com>
2025-03-12 16:06:02 +0900

    0312 DockerFile 설정

--- src/test/java/com/example/demo/SpringMybatisGradleApplicationTests.java
+++ src/test/java/com/example/demo/SpringMybatisGradleApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan({"data.*","*.controller","com.example.demo","naver.storage","naver.aiservice"})
+@MapperScan({"data.mapper"})
 class SpringMybatisGradleApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
-
 }
